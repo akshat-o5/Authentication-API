ccffb9059ebfabf4372700fb4af7ba1a4ec4a610
Akshat Gupta <a.akshat903@gmail.com>
2024-01-09 02:21:03 +0530

    files added

--- authentication/auth_api/views.py
+++ authentication/auth_api/views.py
@@ -5,10 +5,13 @@ from rest_framework.views import APIView
 from auth_api.serializers import HumanRegisterationSerializer, HumanSerializer, HumanLoginSerializer
 from auth_api.models import Human, HumanManager
 from django.contrib.auth import authenticate
+from auth_api.renderers import HumanRenderer
 
 # Create your views here.
 
 class HumanRegisterationView(APIView):
+
+    renderer_classes = [HumanRenderer]
 
     def get(self, request, format=None):
         humans = Human.objects.all()
@@ -32,13 +35,16 @@ class HumanRegisterationView(APIView):
 
 
 class HumanLoginView(APIView):
+
+    renderer_classes = [HumanRenderer]
+
     def post(self, request, forat=None):
         serializer = HumanLoginSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             email=serializer.data.get('email')
             password=serializer.data.get('password')
             human = authenticate(email=email, password=password)
-            if user is not None:
+            if human is not None:
                 return Response({'msg':'Login Successful'}, status=status.HTTP_200_OK)
             else:
                 return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)      
