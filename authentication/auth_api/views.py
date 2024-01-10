109504ef56f08303d3cf923dcb7091a8bb3a8774
Akshat Gupta <a.akshat903@gmail.com>
2024-01-10 21:08:36 +0530

    files added

--- authentication/auth_api/views.py
+++ authentication/auth_api/views.py
@@ -11,7 +11,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 # Create your views here.
 
 
-
+# Generating Token Manualy
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
 
@@ -35,8 +35,9 @@ class HumanRegisterationView(APIView):
     def post(self, request, format=None):
         serializer = HumanRegisterationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response({'msg':'Registeration Successful'},status=status.HTTP_201_CREATED)
+            human = serializer.save()
+            token = get_tokens_for_user(human)
+            return Response({'token':token, 'msg':'Registeration Successful'},status=status.HTTP_201_CREATED)
             
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
@@ -59,7 +60,8 @@ class HumanLoginView(APIView):
             password=serializer.data.get('password')
             human = authenticate(email=email, password=password)
             if human is not None:
-                return Response({'msg':'Login Successful'}, status=status.HTTP_200_OK)
+                token = get_tokens_for_user(human)
+                return Response({'token':token, 'msg':'Login Successful'}, status=status.HTTP_200_OK)
             else:
                 return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_404_NOT_FOUND)      
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)         
